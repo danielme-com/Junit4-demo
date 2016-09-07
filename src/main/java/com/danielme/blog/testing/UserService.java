dd9d9ca3e804544537650c7c9955a99a8beb14f7
Daniel Medina <danielme_com@yahoo.com>
2016-09-07 17:58:58 +0200

    first commit

---
+++ src/main/java/com/danielme/blog/testing/UserService.java
@@ -0,0 +1,29 @@
+package com.danielme.blog.testing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum UserService {
+
+	INSTANCE;
+
+	private final String[] users = { "Bombur", "Bofur", "Bifur", "Nori", "Ori", "Dori", "Thorin", "Balin", "Dwalin",
+			"Oin", "Gloin", "Fili", "Kili" };
+
+	public List<String> getUsersByName(String name) {
+		List<String> res = new ArrayList<String>();
+		if (name != null) {
+			for (String user : users) {
+				if (user.toUpperCase().contains(name.toUpperCase())) {
+					res.add(user);
+				}
+			}
+		}
+		return res;
+	}
+
+	public String getUserByPosition(int pos) {
+		return users[pos];
+	}
+
+}
---
+++ src/test/java/com/danielme/blog/testing/UserServiceTest.java
@@ -0,0 +1,98 @@
+package com.danielme.blog.testing;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+@RunWith(JUnitParamsRunner.class)
+public class UserServiceTest {
+
+    private static final Logger logger = Logger.getLogger(UserServiceTest.class);
+    
+    @Rule
+    public ExpectedException exception = ExpectedException.none();    
+    
+    @Rule
+    public Timeout timeout = Timeout.millis(5000);
+    
+    @BeforeClass
+    public static void setUpClass() {
+        logger.info("");
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        logger.info("");
+    }
+
+    @Before
+    public void setUpTest() {
+        logger.info("");
+    }
+
+    @After
+    public void tearDownTest() {
+        logger.info("");
+    }
+
+    @Test
+    public void getUsersByName() {
+        List<String> usersByName = UserService.INSTANCE.getUsersByName("oin");
+        assertEquals(2, usersByName.size());
+        assertTrue("Gloin not found!!", usersByName.contains("Gloin"));
+        assertTrue("Oin not found!!", usersByName.contains("Oin"));
+    }
+    
+    @Test
+    @Parameters({"oin, 2", "balin, 1", "gimli, 0" })
+    public void getUsersByNameParameterised(String name, int result) {
+        List<String> usersByName = UserService.INSTANCE.getUsersByName(name);
+        assertEquals(result, usersByName.size());        
+    }
+
+    @Test
+    public void getUserByPositionException1() {
+        try {
+            UserService.INSTANCE.getUserByPosition(-1);
+            fail("Exception not thown");
+        } catch (IndexOutOfBoundsException ex) {
+           assertEquals("-1", ex.getMessage());
+        }
+    }
+   
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getUserByPositionException2() {
+        UserService.INSTANCE.getUserByPosition(-1);
+    }
+    
+    @Test
+    public void getUserByPositionException3() {
+        exception.expect(IndexOutOfBoundsException.class);
+        exception.expectMessage("-1");
+     
+        UserService.INSTANCE.getUserByPosition(-1);
+    }
+
+    /*@Test(timeout=5000)    
+    public void test2() {
+       for (int i = 0; i < 99999; i++)
+        logger.info("");
+    }*/
+
+}
