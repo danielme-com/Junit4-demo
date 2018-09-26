6d7e78eb88f3b2b57185f34afe2cbf4c3d5d6a53
Daniel Medina <danielme_com@yahoo.com>
2018-09-26 20:45:41 +0200

    use blank spaces instead of tabs

--- src/main/java/com/danielme/blog/testing/UserService.java
+++ src/main/java/com/danielme/blog/testing/UserService.java
@@ -5,25 +5,25 @@ import java.util.List;
 
 public enum UserService {
 
-	INSTANCE;
+    INSTANCE;
 
-	private final String[] users = { "Bombur", "Bofur", "Bifur", "Nori", "Ori", "Dori", "Thorin", "Balin", "Dwalin",
-			"Oin", "Gloin", "Fili", "Kili" };
+    private final String[] users = { "Bombur", "Bofur", "Bifur", "Nori", "Ori", "Dori", "Thorin",
+            "Balin", "Dwalin", "Oin", "Gloin", "Fili", "Kili" };
 
-	public List<String> getUsersByName(String name) {
-		List<String> res = new ArrayList<String>();
-		if (name != null) {
-			for (String user : users) {
-				if (user.toUpperCase().contains(name.toUpperCase())) {
-					res.add(user);
-				}
-			}
-		}
-		return res;
-	}
+    public List<String> getUsersByName(String name) {
+        List<String> res = new ArrayList<String>();
+        if (name != null) {
+            for (String user : users) {
+                if (user.toUpperCase().contains(name.toUpperCase())) {
+                    res.add(user);
+                }
+            }
+        }
+        return res;
+    }
 
-	public String getUserByPosition(int pos) {
-		return users[pos];
-	}
+    public String getUserByPosition(int pos) {
+        return users[pos];
+    }
 
 }
