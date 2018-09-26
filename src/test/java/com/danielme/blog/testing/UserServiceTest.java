f2995a8993dd13c157e9a71300c99f7d3ba75238
Daniel Medina <danielme_com@yahoo.com>
2018-09-26 20:45:08 +0200

    AssertJ with Java 8

--- src/test/java/com/danielme/blog/testing/UserServiceTest.java
+++ src/test/java/com/danielme/blog/testing/UserServiceTest.java
@@ -19,18 +19,20 @@ import junitparams.Parameters;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(JUnitParamsRunner.class)
 public class UserServiceTest {
 
     private static final Logger logger = Logger.getLogger(UserServiceTest.class);
-    
+
     @Rule
-    public ExpectedException exception = ExpectedException.none();    
-    
+    public ExpectedException exception = ExpectedException.none();
+
     @Rule
     public Timeout timeout = Timeout.millis(5000);
-    
+
     @BeforeClass
     public static void setUpClass() {
         logger.info("");
@@ -58,12 +60,29 @@ public class UserServiceTest {
         assertTrue("Gloin not found!!", usersByName.contains("Gloin"));
         assertTrue("Oin not found!!", usersByName.contains("Oin"));
     }
-    
+
     @Test
-    @Parameters({"oin, 2", "balin, 1", "gimli, 0" })
+    @Parameters({ "oin, 2", "balin, 1", "gimli, 0" })
     public void getUsersByNameParameterised(String name, int result) {
         List<String> usersByName = UserService.INSTANCE.getUsersByName(name);
-        assertEquals(result, usersByName.size());        
+        assertEquals(result, usersByName.size());
+    }
+
+    @Test
+    public void getUsersByNameAssertJ() {
+        List<String> usersByName = UserService.INSTANCE.getUsersByName("oin");
+        assertThat(usersByName)
+        .hasSize(2)
+        .contains("Gloin", "Oin");
+    }
+
+    @Test
+    public void testExceptionAssertJ() {
+        assertThatThrownBy(() -> {
+            UserService.INSTANCE.getUserByPosition(-1);
+        })
+        .isInstanceOf(IndexOutOfBoundsException.class)
+        .hasMessageContaining("-1");
     }
 
     @Test
@@ -72,27 +91,26 @@ public class UserServiceTest {
             UserService.INSTANCE.getUserByPosition(-1);
             fail("Exception not thown");
         } catch (IndexOutOfBoundsException ex) {
-           assertEquals("-1", ex.getMessage());
+            assertEquals("-1", ex.getMessage());
         }
     }
-   
+
     @Test(expected = IndexOutOfBoundsException.class)
     public void getUserByPositionException2() {
         UserService.INSTANCE.getUserByPosition(-1);
     }
-    
+
     @Test
     public void getUserByPositionException3() {
         exception.expect(IndexOutOfBoundsException.class);
         exception.expectMessage("-1");
-     
+
         UserService.INSTANCE.getUserByPosition(-1);
     }
 
-    /*@Test(timeout=5000)    
-    public void test2() {
-       for (int i = 0; i < 99999; i++)
-        logger.info("");
-    }*/
+    /*
+     * @Test(timeout=5000) public void test2() { for (int i = 0; i < 99999; i++)
+     * logger.info(""); }
+     */
 
 }
